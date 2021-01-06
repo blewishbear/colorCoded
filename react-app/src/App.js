b1e17acf804f3e774e3068decae0b2b351d00feb
Mathias <mathiasanderson88@gmail.com>
2021-01-06 13:22:50 -0800

    Splash page and signup/in working

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter, Route } from "react-router-dom";
-import LoginForm from "./components/auth/LoginForm";
-import SignUpForm from "./components/auth/SignUpForm";
-import NavBar from "./components/NavBar";
-import ProtectedRoute from "./components/auth/ProtectedRoute";
-import UsersList from "./components/UsersList";
-import User from "./components/User";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import NavBar from "./components/SplashPage/Navbar/index.js";
+import HomePage from "./components/SplashPage/HomePage"
+import ProtectedRoute from "./components/SplashPage/auth/ProtectedRoute";
+
 import { authenticate } from "./services/auth";
 
 function App() {
@@ -25,30 +24,52 @@ function App() {
   if (!loaded) {
     return null;
   }
-
   return (
     <BrowserRouter>
-      <NavBar setAuthenticated={setAuthenticated} />
-      <Route path="/login" exact={true}>
-        <LoginForm
-          authenticated={authenticated}
-          setAuthenticated={setAuthenticated}
-        />
-      </Route>
-      <Route path="/sign-up" exact={true}>
-        <SignUpForm authenticated={authenticated} setAuthenticated={setAuthenticated} />
-      </Route>
-      <ProtectedRoute path="/users" exact={true} authenticated={authenticated}>
-        <UsersList/>
-      </ProtectedRoute>
-      <ProtectedRoute path="/users/:userId" exact={true} authenticated={authenticated}>
-        <User />
-      </ProtectedRoute>
-      <ProtectedRoute path="/" exact={true} authenticated={authenticated}>
-        <h1>My Home Page</h1>
-      </ProtectedRoute>
+    <div>Hello</div>
+      <NavBar authenticated={authenticated} setAuthenticated={setAuthenticated} />
+      <Switch>
+        <Route path="/" exact={true}>
+          <HomePage></HomePage>
+        </Route>
+        {/* <ProtectedRoute exact={true} path="/listings/create" authenticated={authenticated}>
+          <CreateHouseForm user={authenticated} />
+        </ProtectedRoute> */}
+        {/* <Route path='/listings/:id' exact={true}>
+          <HouseProfilePage />
+        </Route>
+        <Route path="/listings">
+          <ListingsIdx />
+        </Route> */}
+      </Switch>
     </BrowserRouter>
   );
 }
+
+
+  // return (
+  //   <BrowserRouter>
+  //     <NavBar setAuthenticated={setAuthenticated} />
+  //     <Route path="/login" exact={true}>
+  //       <LoginForm
+  //         authenticated={authenticated}
+  //         setAuthenticated={setAuthenticated}
+  //       />
+  //     </Route>
+  //     <Route path="/sign-up" exact={true}>
+  //       <SignUpForm authenticated={authenticated} setAuthenticated={setAuthenticated} />
+  //     </Route>
+  //     <ProtectedRoute path="/users" exact={true} authenticated={authenticated}>
+  //       <UsersList/>
+  //     </ProtectedRoute>
+  //     <ProtectedRoute path="/users/:userId" exact={true} authenticated={authenticated}>
+  //       <User />
+  //     </ProtectedRoute>
+  //     <ProtectedRoute path="/" exact={true} authenticated={authenticated}>
+  //       <h1>My Home Page</h1>
+  //     </ProtectedRoute>
+  //   </BrowserRouter>
+  // );
+// }
 
 export default App;
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
+    <div>You</div>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
